56cfe4d7177b80ee98961b4e951f7f79dfcfac9c
Danil <danylo.makhov@nure.ua>
2022-11-09 18:28:44 +0300

    fix 5,6task

--- 06-build-page/index.js
+++ 06-build-page/index.js
@@ -2,6 +2,9 @@ const fs = require('fs');
 const path = require('path');
 const { start } = require('repl');
 
+const styles = path.join(__dirname, "styles")
+const bundleDir = path.join(__dirname, 'project-dist', 'style.css')
+
 async function directory() {
   await fs.mkdir(
     path.join(__dirname, 'project-dist'),
@@ -14,40 +17,32 @@ async function directory() {
     () => {}
   );
 }
+async function generateBundle () {
+  const data = await copyCss(styles)
+  createBundle(bundleDir, data)
+}
+const copyCss = (paths) => {
+  return new Promise((res, rej) => {
+    fs.readdir(paths, {withFileTypes: true}, (err, data) => {
+      if(err) throw err 
+      res (
+        data.filter(dat => dat.isFile()).filter(dat => path.extname(dat.name) == '.css').map(dat => path.join(paths, dat.name))
+      )
+    })
+  })
+}
+const createBundle = async(bundle, file) => {
+  const stream = fs.createWriteStream(bundle)
+  file.forEach(element => {
+    const stream1 = fs.createReadStream(element)
+    stream1.pipe(stream)
+  });
+}
+
+
 
 function starst() {
-  fs.readdir(path.join(__dirname, 'styles'), (err, data) => {
-    if (err) throw err;
-    data.forEach((elem) => {
-      fs.stat(path.join(__dirname, 'styles', elem), (err, stat) => {
-        if (err) throw err;
-        // eslint-disable-next-line no-empty
-        if (stat.isFile() && elem.split('.')[1] === 'css') {
-          fs.readFile(
-            path.join(__dirname, 'styles', elem),
-            function (err, content) {
-              if (err) {
-                throw err;
-              } else {
-                const datav = Buffer.from(content);
-                fs.appendFile(
-                  path.join(__dirname, 'project-dist', 'style.css'),
-                  datav,
-                  (errr) => {
-                    // console.log('стили скопированы');
-                    if (errr) {
-                      throw errr;
-                    }
-                  }
-                );
-              }
-            }
-          );
-        }
-      });
-    });
-  });
-
+  generateBundle()
   /* styles copy top */
 
   /* assets copy */
